ea10f5feddcf15c54150bdf412cd9667666abdc6
John Hoffer <hoffexi@gmail.com>
2017-01-17 17:12:13 -0500

    bfly interaction

---
+++ bfly/main.js
@@ -0,0 +1,14 @@
+function main_init(){
+  var source_terms = {
+    depth: 1774,
+    width: 2000,
+    height: 2000,
+    experiment: "root",
+    sample: "root",
+    dataset: "jan_push",
+    base_channel: "images_2kx2k.h5",
+    over_channel: "8x_downsampled_segmentation.h5",
+    server: 'viper.krash.net:2016'
+  };
+  load = new Loader(source_terms);
+}
