f6eddd62f82a9411ff14760e7dcb1e9f300e9fa4
John Hoffer <hoffexi@gmail.com>
2017-02-01 11:23:53 -0500

    loader example

--- bfly/main.js
+++ bfly/main.js
@@ -5,13 +5,25 @@ function main_init(){
     height: 2000,
     experiment: "root",
     sample: "root",
-    dataset: "jan_push",
+    dataset: "iarpa2016_12",
     base_channel: "images_2kx2k.h5",
     over_channel: "8x_downsampled_segmentation.h5",
     server: 'http://viper.krash.net:2016'
   };
   load = new Loader(source_terms);
-  load.r --;
-  base_0 = load.getTile('base',0);
-  view = new Viewer(base_0);
+
+  function sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  async function loadall(level){
+    base = load.getTile('base',level);
+    view = new Viewer(base);
+    await sleep(500);
+    if (level+1 < load.rlen){
+      loadall(level+1);
+    }
+  }
+  load.r = 0;
+  loadall(0)
 }
